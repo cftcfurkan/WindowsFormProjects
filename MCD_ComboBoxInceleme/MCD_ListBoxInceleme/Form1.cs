7cec2c688f3651f6d4f89b9f66bbacee4962f9d5
cftcfurkan <90473630+cftcfurkan@users.noreply.github.com>
2022-02-10 19:02:13 +0300

    Add files via upload

---
+++ MCD_ComboBoxInceleme/MCD_ListBoxInceleme/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_ListBoxInceleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //listboxDoldurV1();
+            listboxDoldurV2();
+        }
+
+        private void listboxDoldurV2()
+        {
+            lstUrunler.DisplayMember = "urunAdi";
+            //lstUrunler.ValueMember = "id";
+            lstUrunler.DataSource = database.UrunTablo;
+        }
+
+        private void listboxDoldurV1()
+        {
+            foreach (var item in database.UrunTablo)
+            {
+                lstUrunler.Items.Add(item);
+            }
+        }
+
+        private void lstUrunler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int urunSecimAdet = lstUrunler.SelectedItems.Count;
+            if (urunSecimAdet == 1)
+            {
+                ListBox lbox = (ListBox)sender;
+                //lbox = sender as ListBox;
+                urun u1 = (urun)lbox.SelectedItem;
+
+                pctUrunResim.Image = Image.FromFile(u1.urunResim);
+                pctUrunResim.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                txtUrunAdi.Text = u1.urunAdi;
+                txtUrunKategori.Text = u1.urunKategori;
+                txtStokAdet.Text = u1.stokAdet.ToString();
+                txtYazar.Text = u1.yazar;
+                txtUrunAciklama.Text = u1.aciklama;
+            }
+            else if(urunSecimAdet > 1)
+            {
+                List<urun> secilenUrunListe = new List<urun>();
+                foreach (var item in lstUrunler.SelectedItems)
+                {
+                    secilenUrunListe.Add(item as urun);
+                }
+
+                urunSecimKullanici secimKullanici = new urunSecimKullanici(secilenUrunListe.ToArray());
+                secimKullanici.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen detayını görmek istediğiniz ürünü seçiniz.", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        
+    }
+}
---
+++ MCD_ComboBoxInceleme/MCD_ListBoxInceleme/urunSecimKullanici.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_ListBoxInceleme
+{
+    public partial class urunSecimKullanici : Form
+    {
+        urun[] urunListe;
+        public urunSecimKullanici(urun[] secilenUrunler)
+        {
+            InitializeComponent();
+            urunListe = secilenUrunler;
+
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void urunSecimKullanici_Load(object sender, EventArgs e)
+        {
+            foreach (var item in urunListe)
+            {
+                Button btn = new Button();
+                btn.Text = item.urunAdi;
+                btn.Name = "btn" + item.id.ToString();
+                btn.Size = new System.Drawing.Size(122, 37);
+                btn.Tag = item;
+                btn.Click += Btn_Click;
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            urun u1 = btn.Tag as urun;
+
+            Form F1 = Application.OpenForms["Form1"];
+            GroupBox F1GBOX = (GroupBox)F1.Controls["groupBox1"];
+            ((PictureBox)F1GBOX.Controls["pctUrunResim"]).Image = Image.FromFile(u1.urunResim);
+            ((TextBox)F1GBOX.Controls["txtUrunAdi"]).Text = u1.urunAdi;
+            ((TextBox)F1GBOX.Controls["txtUrunKategori"]).Text = u1.urunKategori;
+            ((TextBox)F1GBOX.Controls["txtStokAdet"]).Text = u1.stokAdet.ToString();
+            ((TextBox)F1GBOX.Controls["txtYazar"]).Text = u1.yazar;
+            ((TextBox)F1GBOX.Controls["txtUrunAciklama"]).Text = u1.aciklama;
+            this.Close();
+    
+        }
+    }
+}
