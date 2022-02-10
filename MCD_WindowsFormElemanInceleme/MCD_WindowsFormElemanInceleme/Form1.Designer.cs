359e305f1ab81b2a25b552ba3ccbe6e827b454a2
cftcfurkan <90473630+cftcfurkan@users.noreply.github.com>
2022-02-10 19:04:55 +0300

    Add files via upload

---
+++ MCD_WindowsFormElemanInceleme/MCD_WindowsFormElemanInceleme/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace MCD_WindowsFormElemanInceleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnResimSec = new System.Windows.Forms.Button();
+            this.chkEkGaranti = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUrunTanim = new System.Windows.Forms.TextBox();
+            this.txtUrunKod = new System.Windows.Forms.TextBox();
+            this.cmbUrunKategori = new System.Windows.Forms.ComboBox();
+            this.cltUrunOzellikleri = new System.Windows.Forms.CheckedListBox();
+            this.txtAciklama = new System.Windows.Forms.RichTextBox();
+            this.cmbUrunRenk = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pctUrunResim = new System.Windows.Forms.PictureBox();
+            this.bnKaydet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUrunResim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnResimSec
+            // 
+            this.btnResimSec.Location = new System.Drawing.Point(27, 375);
+            this.btnResimSec.Name = "btnResimSec";
+            this.btnResimSec.Size = new System.Drawing.Size(75, 23);
+            this.btnResimSec.TabIndex = 0;
+            this.btnResimSec.Text = "Resim Seç";
+            this.btnResimSec.UseVisualStyleBackColor = true;
+            // 
+            // chkEkGaranti
+            // 
+            this.chkEkGaranti.AutoSize = true;
+            this.chkEkGaranti.Location = new System.Drawing.Point(139, 340);
+            this.chkEkGaranti.Name = "chkEkGaranti";
+            this.chkEkGaranti.Size = new System.Drawing.Size(97, 17);
+            this.chkEkGaranti.TabIndex = 1;
+            this.chkEkGaranti.Text = "Ek garanti 2 Yıl";
+            this.chkEkGaranti.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürün Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ürün Kategori";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ürün Açıklama";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Ürün Özellikleri";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Ürün Kod";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Ürün Renk";
+            // 
+            // txtUrunTanim
+            // 
+            this.txtUrunTanim.Location = new System.Drawing.Point(139, 25);
+            this.txtUrunTanim.Name = "txtUrunTanim";
+            this.txtUrunTanim.Size = new System.Drawing.Size(177, 20);
+            this.txtUrunTanim.TabIndex = 3;
+            // 
+            // txtUrunKod
+            // 
+            this.txtUrunKod.Location = new System.Drawing.Point(139, 176);
+            this.txtUrunKod.Name = "txtUrunKod";
+            this.txtUrunKod.Size = new System.Drawing.Size(177, 20);
+            this.txtUrunKod.TabIndex = 3;
+            // 
+            // cmbUrunKategori
+            // 
+            this.cmbUrunKategori.FormattingEnabled = true;
+            this.cmbUrunKategori.Location = new System.Drawing.Point(139, 48);
+            this.cmbUrunKategori.Name = "cmbUrunKategori";
+            this.cmbUrunKategori.Size = new System.Drawing.Size(177, 21);
+            this.cmbUrunKategori.TabIndex = 4;
+            // 
+            // cltUrunOzellikleri
+            // 
+            this.cltUrunOzellikleri.FormattingEnabled = true;
+            this.cltUrunOzellikleri.Location = new System.Drawing.Point(139, 75);
+            this.cltUrunOzellikleri.Name = "cltUrunOzellikleri";
+            this.cltUrunOzellikleri.Size = new System.Drawing.Size(177, 94);
+            this.cltUrunOzellikleri.TabIndex = 5;
+            // 
+            // txtAciklama
+            // 
+            this.txtAciklama.Location = new System.Drawing.Point(139, 238);
+            this.txtAciklama.Name = "txtAciklama";
+            this.txtAciklama.Size = new System.Drawing.Size(177, 96);
+            this.txtAciklama.TabIndex = 6;
+            this.txtAciklama.Text = "";
+            // 
+            // cmbUrunRenk
+            // 
+            this.cmbUrunRenk.FormattingEnabled = true;
+            this.cmbUrunRenk.Location = new System.Drawing.Point(139, 202);
+            this.cmbUrunRenk.Name = "cmbUrunRenk";
+            this.cmbUrunRenk.Size = new System.Drawing.Size(177, 21);
+            this.cmbUrunRenk.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bnKaydet);
+            this.groupBox1.Controls.Add(this.pctUrunResim);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnResimSec);
+            this.groupBox1.Controls.Add(this.cmbUrunKategori);
+            this.groupBox1.Controls.Add(this.chkEkGaranti);
+            this.groupBox1.Controls.Add(this.cltUrunOzellikleri);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbUrunRenk);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtUrunTanim);
+            this.groupBox1.Controls.Add(this.txtUrunKod);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtAciklama);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 587);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ürün Liste";
+            // 
+            // pctUrunResim
+            // 
+            this.pctUrunResim.Location = new System.Drawing.Point(139, 363);
+            this.pctUrunResim.Name = "pctUrunResim";
+            this.pctUrunResim.Size = new System.Drawing.Size(177, 166);
+            this.pctUrunResim.TabIndex = 9;
+            this.pctUrunResim.TabStop = false;
+            // 
+            // bnKaydet
+            // 
+            this.bnKaydet.Location = new System.Drawing.Point(241, 544);
+            this.bnKaydet.Name = "bnKaydet";
+            this.bnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.bnKaydet.TabIndex = 10;
+            this.bnKaydet.Text = "Kaydet";
+            this.bnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(390, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(518, 577);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // OFD
+            // 
+            this.OFD.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 633);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUrunResim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResimSec;
+        private System.Windows.Forms.CheckBox chkEkGaranti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUrunTanim;
+        private System.Windows.Forms.TextBox txtUrunKod;
+        private System.Windows.Forms.ComboBox cmbUrunKategori;
+        private System.Windows.Forms.CheckedListBox cltUrunOzellikleri;
+        private System.Windows.Forms.RichTextBox txtAciklama;
+        private System.Windows.Forms.ComboBox cmbUrunRenk;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bnKaydet;
+        private System.Windows.Forms.PictureBox pctUrunResim;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog OFD;
+    }
+}
+
---
+++ MCD_WindowsFormElemanInceleme/MCD_WindowsFormElemanInceleme/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_WindowsFormElemanInceleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void sistemGirisForm()
+        {
+            sistemGiris sistemGirisForm = new sistemGiris();
+            sistemGirisForm.ShowDialog();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            sistemGirisForm();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormElemanlariDoldur();
+        }
+
+        private void FormElemanlariDoldur()
+        {
+            cltUrunOzellikleri.Items.AddRange(Helper.urunOzellikListe.ToArray());
+
+            //combobox değer atama - 1
+            cmbUrunKategori.Items.AddRange(Helper.kategoriListe.ToArray());
+
+            //combobox değer atama - 2
+            cmbUrunRenk.Items.AddRange(Helper.urunRenk.ToArray());
+
+        }
+    }
+}
